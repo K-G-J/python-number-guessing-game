4c82b742a21e15826cdfd4c84d42359b8b5ecbb1
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-03 18:59:37 -0500

    moved get_guess func to global scope

--- guessing_game.py
+++ guessing_game.py
@@ -6,40 +6,42 @@ Project 1 - Number Guessing Game
 
 import random
 
+answer = random.randint(1, 10)
 
-def start_game():
-    name = input("What is your name?  ")
+attempts = 0
 
-    print(f"Welcome to the number guessing game {name}! 😄")
+name = input("What is your name?  ")
 
-    answer = random.randint(1, 10)
+print(f"Welcome to the number guessing game {name}! 😄")
 
-    attempts = 0
 
-    def get_guess():
-      # TODO fix attempt counter
-        try:
-            guess = int(
-                input("Try to guess the random number between 1 and 10.  "))
-            attempts += 1
-            if guess > 10 or guess < 1:
-                print(
-                    f"{guess} is out of range 😕 the random number is between 1 and 10")
-            if guess > answer:
-                print("It's lower ⬇")
-                get_guess()
-            elif guess < answer:
-                print("It's higher ⬆")
-                get_guess()
-            else:
-                print(
-                    f"Got it! 🥳 \n It took {attempts} attempts to get the correct number \n Game over 👋. \n Thank you for playing {name} 😄")
-        except ValueError as err:
-            print("Oh no! That's not a valid number 😕. Try again...")
-            print(f"({err})")
+def get_guess():
+    global attempts
+    attempts += 1
+    try:
+        guess = int(
+            input("Try to guess the random number between 1 and 10.  "))
+        if guess > 10 or guess < 1:
+            print(
+                f"{guess} is out of range 😕 the random number is between 1 and 10")
+        if guess > answer:
+            print("It's lower ⬇")
+            get_guess()
+        elif guess < answer:
+            print("It's higher ⬆")
             get_guess()
         else:
-            return guess
+            print(
+                f"Got it! 🥳 \n It took {attempts} attempts to get the correct number \n Game over 👋. \n Thank you for playing {name} 😄")
+    except ValueError as err:
+        print("Oh no! That's not a valid number 😕. Try again...")
+        print(f"({err})")
+        get_guess()
+    else:
+        return guess
+
+
+def start_game():
 
     get_guess()
 
