849ae99f6998f6709a47d42e5e556cdbd30be734
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-04 16:15:15 -0500

    added high_score calc and display

--- guessing_game.py
+++ guessing_game.py
@@ -11,8 +11,11 @@ name = input("What is your name?  ")
 
 print(f"Welcome to the number guessing game {name}! 😄")
 
+high_score = 0
+
 
 def get_guess(attempts, answer):
+    global high_score
     try:
         guess = int(
             input("Try to guess the random number between 1 and 10.  "))
@@ -31,10 +34,15 @@ def get_guess(attempts, answer):
             attempts += 1
             get_guess(attempts, answer)
         else:
-            # Correct guess
-            # TODO: save and update high_score
+            # Handle correct guess
+            attempts += 1
             print(
+                f"Got it! 🥳 \n It took {attempts} attempt to get the correct number \n Game over 👋. \n Thank you for playing {name} 😄") if attempts == 1 else print(
                 f"Got it! 🥳 \n It took {attempts} attempts to get the correct number \n Game over 👋. \n Thank you for playing {name} 😄")
+            # Save and update high_score
+            if high_score == 0 or attempts < high_score:
+                high_score = attempts
+                print("You set the high score! 🤩")
     except ValueError as err:
         # Handle invalid value guesses
         print("Oh no! That's not a valid number 😕. Try again...")
@@ -46,7 +54,9 @@ def get_guess(attempts, answer):
 
 
 def start_game():
-    # TODO: Display the current high_score
+    # Display current high score
+    if high_score != 0:
+        print(f"The current high score is {high_score} 🤗")
     # Reset attempts and new random number before each round
     attempts = 0
     answer = random.randint(1, 10)
