617812cff6568d71f5d0d320308a7bf00119b0ac
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-05 10:18:42 -0500

    removed recursion from get_guess func

--- guessing_game.py
+++ guessing_game.py
@@ -11,41 +11,40 @@ high_score = 0
 
 def get_guess(attempts, answer, name):
     global high_score
-    try:
-        guess = int(
-            input("Try to guess the random number between 1 and 10.  "))
-        if guess > 10 or guess < 1:
-            # Handle out of range guesses
-            print(
-                f"{guess} is out of range 😕 the random number is between 1 and 10")
+    while True:
+        try:
             attempts += 1
-            get_guess(attempts, answer, name)
-        elif guess > answer:
-            print("It's lower ⬇")
-            attempts += 1
-            get_guess(attempts, answer, name)
-        elif guess < answer:
-            print("It's higher ⬆")
-            attempts += 1
-            get_guess(attempts, answer, name)
-        else:
-            # Handle correct guess
-            attempts += 1
-            print(
-                f"Got it! 🥳 \n It took {attempts} attempt to get the correct number \n Game over 👋. \n Thank you for playing {name} 😄") if attempts == 1 else print(
-                f"Got it! 🥳 \n It took {attempts} attempts to get the correct number \n Game over 👋. \n Thank you for playing {name} 😄")
-            # Save and update high_score
-            if high_score == 0 or attempts < high_score:
-                high_score = attempts
-                print("You set the high score! 🤩")
-    except ValueError as err:
-        # Handle invalid value guesses
-        print("Oh no! That's not a valid number 😕. Try again...")
-        print(f"({err})")
-        attempts += 1
-        get_guess(attempts, answer, name)
-    else:
-        return guess
+            guess = int(
+                input("Try to guess the random number between 1 and 10.  "))
+            if guess > 10 or guess < 1:
+                # Handle out of range guesses
+                print(
+                    f"{guess} is out of range 😕 the random number is between 1 and 10")
+                continue
+            elif guess > answer:
+                print("It's lower ⬇")
+                continue
+            elif guess < answer:
+                print("It's higher ⬆")
+                continue
+            elif guess == answer:
+                # Handle correct guess
+                print(
+                    f"Got it! 🥳 \n It took {attempts} attempt to get the correct number \n Game over 👋. \n Thank you for playing {name} 😄") if attempts == 1 else print(
+                    f"Got it! 🥳 \n It took {attempts} attempts to get the correct number \n Game over 👋. \n Thank you for playing {name} 😄")
+                # Save and update high_score
+                if high_score == 0 or attempts < high_score:
+                    high_score = attempts
+                    print("You set the high score! 🤩")
+                break
+            else:
+                print("Oops 😕 ... something went wrong. Please try again.")
+                continue
+        except ValueError as err:
+            # Handle invalid value guesses
+            print("Oh no! That's not a valid number 😕. Try again...")
+            print(f"({err})")
+            continue
 
 
 def start_game(name):
